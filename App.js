ecf3eee2ba12c3629f358baf5049ef1a02bd06a5
Meran Arumugam <meran.arumugam@gmail.com>
2023-09-23 10:41:32 -0400

    Update App.js
    Added Firestone/firebase

--- App.js
+++ App.js
@@ -8,7 +8,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
+//importing firestore database
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const App = () => {
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD066RtevfsjLWCvKSZ5FGmPuKjyN9YNg0",
+    authDomain: "chatapp-ac3e9.firebaseapp.com",
+    projectId: "chatapp-ac3e9",
+    storageBucket: "chatapp-ac3e9.appspot.com",
+    messagingSenderId: "253472736419",
+    appId: "1:253472736419:web:617f71bf86eff5175b2b16"
+  };
+  // initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -19,6 +38,7 @@ const App = () => {
           component={Start}
         />
         <Stack.Screen
+          {...props => <Chat db={db} {...props} />}
           name="Chat"
           component={Chat}
         />
