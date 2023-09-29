57541a297c8ac71e9517bb07287347ed58719df7
Meran Arumugam <meran.arumugam@gmail.com>
2023-09-29 08:15:14 -0400

    Update Chat.js
    Added Input ToolBar, added isConnected to route Chat, added Async Storage, added code for messages to be displayed online and offline

--- components/Chat.js
+++ components/Chat.js
@@ -1,33 +1,60 @@
 import { useState, useEffect } from "react";
 import { collection, addDoc, onSnapshot, query, orderBy } from 'firebase/firestore';
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { StyleSheet, View, Text, KeyboardAvoidingView, FlatList } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
     const { name, color, userID } = route.params;
 
     //create a messages state
     const [messages, setMessages] = useState([]);
 
+    let unsubMessages;
+
     //a simulated user message and the new system message
     useEffect(() => {
         navigation.setOptions({ title: name });
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-        const unsubMessages = onSnapshot(q, (docs) => {
-            let newMessages = [];
-            docs.forEach(doc => {
-                newMessages.push({
-                id: doc.id,
-                ...doc.data(),
-                createdAt: new Date(doc.data().createdAt.toMillis())
-                })
-            })
-            setMessages(newMessages);
-        })
+        if (isConnected === true) {
+            // unregister current onSnapshot() listener to avoid registering multiple listeners when
+            // useEffect code is re-executed.
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+            unsubMessages = onSnapshot(
+                query(collection(db, "messages"), orderBy("createdAt", "desc")),
+                (documentsSnapshot) => {
+                    let newMessages = [];
+                    documentsSnapshot.forEach((doc) => {
+                    // shape the messages to match what gifted chat expects
+                    newMessages.push({
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis()),
+                    });
+                });
+                cacheMessages(newMessages);
+                setMessages(newMessages);
+                }
+            );
+        } else loadCachedMessages();
+
         return () => {
             if (unsubMessages) unsubMessages();
+        };
+    }, [isConnected]);
+
+    const loadCachedMessages = async () => {
+        const cachedMessages = await AsyncStorage.getItem("messages") || [];
+        setMessages(JSON.parse(cachedMessages));
+    }
+    
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.message);
         }
-    }, []);
+    }
 
     //called the onSend()
     const onSend = (newMessages) => {
@@ -36,7 +63,7 @@ const Chat = ({ route, navigation, db }) => {
 
     //llows you to alter how message bubbles are displayed
     const renderBubble = (props) => {
-        return <Bubble
+        return (<Bubble
             {...props}
             wrapperStyle={{
                 right: {
@@ -47,8 +74,18 @@ const Chat = ({ route, navigation, db }) => {
                 }
             }}
         />
-    }
-    console.log('color:', color)
+        );
+    };
+
+    const renderInputToolbar = (props) => {
+        if (isConnected) {
+            return <InputToolbar {...props} />;
+        } else {
+            return null;
+        }
+    };
+    
+
     return (
         <View style={[styles.container, { backgroundColor: color }]}>
             <GiftedChat
@@ -59,12 +96,13 @@ const Chat = ({ route, navigation, db }) => {
                     name: name,
                 }}
                 renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
             />
-            {/*<FlatList
+            {/*{<FlatList
                 data={messages}
                 renderItem={({ message }) =>
                 <Text>{message.name}: {message.messages.join(", ")}</Text>}
-            />*/}
+            />}*/}
             { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
             <Text>Hello User!</Text>
         </View>
@@ -74,7 +112,6 @@ const Chat = ({ route, navigation, db }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#474056'
     },
 });
 
